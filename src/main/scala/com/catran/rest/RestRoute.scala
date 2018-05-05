042f170a14897dceba3d2dfd28ee39eb7330ae74
aandrosov <coffee-04@yandex.ru>
2018-05-05 14:32:36 +0300

    added classes for the creating rest server

---
+++ src/main/scala/com/catran/rest/RestRoute.scala
@@ -0,0 +1,63 @@
+package com.catran.rest
+
+
+
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server
+import akka.http.scaladsl.server.{Directives, StandardRoute}
+import com.tinkoff.data.DataHandler
+import com.tinkoff.options.ApplicationContext
+
+/**
+  * Routing for REST API
+  */
+class RestRoute(appContext: ApplicationContext) extends Directives {
+
+
+  def route: server.Route = {
+    pathPrefix("tinkoff") {
+      path("data.getData") {
+        get {
+          val response = dataBuilder.getAllData
+          response.errorMessage match {
+            case None => processSuccess(response.toString)
+            case Some(msg) => processFailure(response.toString, response.responseCode)
+          }
+        }
+      } ~
+        path("data.getBackData") {
+          get {
+            val response = dataBuilder.getBackData
+            response.errorMessage match {
+              case None => processSuccess(response.toString)
+              case Some(msg) => processFailure(response.toString, response.responseCode)
+            }
+          }
+        } ~
+        path("disconnect") {
+          get {
+            val response = dataBuilder.deleteTableAndDisconnect()
+            response.errorMessage match {
+              case None => processSuccess(response.toString)
+              case Some(msg) => processFailure(response.toString, response.responseCode)
+            }
+          }
+        }
+    }
+  }
+
+  private def processSuccess(responseAsJson: String): StandardRoute = {
+    complete(StatusCodes.OK, responseAsJson)
+  }
+
+  private def processFailure(responseAsJson: String, responseCode: Int): StandardRoute = {
+    responseCode match {
+      case StatusCodes.NotFound.intValue => complete(StatusCodes.NotFound.intValue, responseAsJson)
+      case _ => complete(StatusCodes.InternalServerError.intValue, responseAsJson)
+    }
+  }
+}
+
+object RestRoute {
+  def apply(appContext: ApplicationContext): RestRoute = new RestRoute(appContext)
+}
---
+++ src/main/scala/com/catran/rest/RestServer.scala
@@ -0,0 +1,61 @@
+package com.catran.rest
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+import org.apache.log4j.Logger
+import org.joda.time.DateTimeZone
+
+
+/**
+  * the start point of application.
+  */
+class ScalaRestBind (applicationContext: ApplicationContext){
+
+  private val logger: Logger = Logger.getLogger(getClass)
+
+
+  /**
+    * creates the connection to database, adds data, and launch REST server
+    */
+  def launch(): Unit = {
+
+    DateTimeZone.setDefault(DateTimeZone.UTC)
+
+    val options = applicationContext.options
+
+    logger.debug(s"Options applied: '${options}'")
+
+    implicit val system: ActorSystem = ActorSystem("system")
+    implicit val materializer = ActorMaterializer()
+    implicit val executionContext = system.dispatcher // needed for the future flatMap/onComplete in the end
+
+    val route = RestRoute(applicationContext)
+
+    val host = options.rest.host
+    val port = options.rest.port
+
+    Http().bindAndHandle(route.route, host, port)
+    logger.info(s"REST server is now listening on http://$host:$port/...")
+  }
+}
+
+object ScalaRestBind {
+
+  def main(args: Array[String]): Unit = {
+    println(s"Parsing input arguments: '${args.mkString(" ")}'")
+    val applicationOptions = ApplicationOptions(args)
+    println(s"options ${applicationOptions}")
+    val storage = DBUtil(applicationOptions)
+    println(s"storage ${storage}")
+    storage.addTimeData(10000)
+
+    val applicationContext = ApplicationContext(
+      options = applicationOptions,
+      storage = storage
+    )
+
+    val app = new ScalaRestBind(applicationContext)
+    app.launch()
+  }
+}
