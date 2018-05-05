21a7753b24a21559f9294b232b9d219fbbf53645
aandrosov <coffee-04@yandex.ru>
2018-05-05 17:34:52 +0300

    added options

---
+++ src/main/scala/com/catran/options/ApplicationContext.scala
@@ -0,0 +1,13 @@
+package com.catran.options
+
+import com.catran.dao.user.UserDao
+
+/**
+  * contains the different info for using application. Maybe to change
+  */
+case class ApplicationContext(
+                               options: ApplicationOptions,
+                               userDao: UserDao
+                             ) {
+
+}
---
+++ src/main/scala/com/catran/options/ApplicationOptions.scala
@@ -0,0 +1,64 @@
+package com.catran.options
+
+/**
+  * using for application the different options
+  */
+case class ApplicationOptions(
+                               storage: String = "mock",
+                               userTableName: String = "userTable",
+                               rest: RestOptions = RestOptions(),
+                               mysql: MysqlOptions = MysqlOptions()
+                             ) {
+}
+
+object ApplicationOptions {
+  val defaults = new ApplicationOptions()
+
+  /**
+    * Initialize options with given arguments
+    */
+  def apply(args: Array[String]): ApplicationOptions = {
+    if(args.isEmpty) return ApplicationOptions.defaults
+    args.foldLeft(ApplicationOptions()) { (options, arg) =>
+      arg.split("=") match {
+        case Array("storage", value) => options.copy(storage = value)
+        case Array("userTableName", value) => options.copy(userTableName = value)
+
+        case Array("rest.host", value) => options.copy(rest = options.rest.copy(host = value))
+        case Array("rest.port", value) => options.copy(rest = options.rest.copy(port = value.toInt))
+
+        case Array("mysql.host", value) => options.copy(mysql = options.mysql.copy(host = value))
+        case Array("mysql.port", value) => options.copy(mysql = options.mysql.copy(port = value.toInt))
+        case Array("mysql.databaseName", value) => options.copy(mysql = options.mysql.copy(databaseName = value))
+        case Array("mysql.autoReconnect", value) => options.copy(mysql = options.mysql.copy(autoReconnect = value.toBoolean))
+        case Array("mysql.useSSL", value) => options.copy(mysql = options.mysql.copy(useSSL = value.toBoolean))
+        case Array("mysql.useJDBCCompliantTimezoneShift", value) => options.copy(mysql = options.mysql.copy(useJDBCCompliantTimezoneShift = value.toBoolean))
+        case Array("mysql.useLegacyDatetimeCode", value) => options.copy(mysql = options.mysql.copy(useLegacyDatetimeCode = value.toBoolean))
+        case Array("mysql.serverTimezone", value) => options.copy(mysql = options.mysql.copy(serverTimezone = value))
+        case Array("mysql.driver", value) => options.copy(mysql = options.mysql.copy(driver = value))
+        case Array("mysql.username", value) => options.copy(mysql = options.mysql.copy(username = value))
+        case Array("mysql.password", value) => options.copy(mysql = options.mysql.copy(password = value))
+
+        case exc => throw new RuntimeException(s"invalid args: ${exc.mkString}")
+      }
+    }
+  }
+}
+case class RestOptions(
+                        host: String = "localhost",
+                        port: Int = 9080
+                      ){}
+
+case class MysqlOptions(
+                         host: String = "localhost",
+                         port: Int = 3306,
+                         databaseName: String = "tinkoff",
+                         autoReconnect: Boolean = true,
+                         useSSL: Boolean = false,
+                         useJDBCCompliantTimezoneShift: Boolean = true,
+                         useLegacyDatetimeCode:Boolean = false,
+                         serverTimezone: String = "UTC",
+                         driver: String = "com.mysql.cj.jdbc.Driver",
+                         username: String = "root",
+                         password: String = "CrisBad33"
+                       ){}
