4961a4b1fa7774a9363fd685ff6bd32b0ea10412
aandrosov <coffee-04@yandex.ru>
2018-05-05 17:24:15 +0300

    added custom exceptions

---
+++ src/main/scala/com/catran/exception/DaoException.scala
@@ -0,0 +1,20 @@
+package com.catran.exception
+
+/**
+  * Created by Administrator on 5/5/2018.
+  */
+class DaoException(message: String) extends Exception(message){
+
+  def this(message: String, cause: Throwable) {
+    this(message)
+    initCause(cause)
+  }
+
+  def this(cause: Throwable) {
+    this(Option(cause).map(_.toString).orNull, cause)
+  }
+
+  def this() {
+    this(null: String)
+  }
+}
